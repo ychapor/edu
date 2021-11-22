0bf5160f61e4db02e73d823dc00004a7adb935e3
Yura Chapor <yura.chapor@gmail.com>
2021-11-22 02:47:48 +0200

    Add task #1 solution. Add .gitignore

---
+++ task_1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Change the owner of directory and its content.
+#
+# Arguments:
+#   new_owner
+#   directory
+
+NEW_OWNER="$1"
+DIRECTORY="$2"
+
+# Display errors if any.
+err() {
+  echo "$*" >&2
+  exit 1
+}
+
+# Checks if user running script has root permitions
+is_root() {
+  if (( $UID != 0 )); then
+    err "Only root allowed to run this scenario!"
+  fi
+}
+
+# Checks if provided <new_owner> exists in system
+user_exists() {
+  if [[ ! $(id ${NEW_OWNER}) ]]; then
+    err "User does not exist!"
+  fi
+}
+
+# Checks if provided <directory> exists
+dir_exists() {
+  if [[ ! -d ${DIRECTORY} ]]; then
+    err "Directory does not exist or is not a directory!"
+  fi
+}
+
+main() {
+  is_root
+  user_exists
+  dir_exists
+
+  chown -R ${NEW_OWNER}:${NEW_OWNER} ${DIRECTORY}
+}
+
+main "$@"
