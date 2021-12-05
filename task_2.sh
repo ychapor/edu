50010c2691e17d8b7b5bda39eeee5a1b189e8371
Yura Chapor <yura.chapor@gmail.com>
2021-12-05 20:06:52 +0200

    Update task_2.sh

--- task_2.sh
+++ task_2.sh
@@ -2,18 +2,19 @@
 #
 # Scenario of creating database backup.
 #
-# You have to create default configuration file named '.<yourDBname>.cnf'
-# in your home directory in order to run the script.
+# You have to create default configuration file named '.<your_db_name>.cnf'
+# in your home directory or provide pathname of such file below in 'CREDENTIALS' in order to run the script.
 # This file must contain your credentials to access the database.
 #
-# Example of .<yourDBname>.cnf:
+# Example of .<your_db_name>.cnf:
 # [mysqldump]
-# user=mysqluser
-# password=secret
+# user=mysql_user
+# password=mysql_password
 
-# Change these constants for backuping different DBs.
+# Change these constants for backing up different DBs.
+BACKUP_DIR=${HOME}/db_backups
 DB_NAME=test_db
-BACKUP_DIR=$HOME/db_backups
+CREDENTIALS=${HOME}/.${DB_NAME}.cnf
 
 if [[ ! -f /run/mysqld/mysqld.pid ]]; then
   echo "MySQL service is not running!" >&2
@@ -21,5 +22,5 @@ if [[ ! -f /run/mysqld/mysqld.pid ]]; then
 fi
 
 mkdir -p ${BACKUP_DIR}
-mysqldump --defaults-extra-file=${HOME}/.${DB_NAME}.cnf --no-tablespaces ${DB_NAME} \
+mysqldump --defaults-extra-file=CREDENTIALS --no-tablespaces ${DB_NAME} \
   > ${BACKUP_DIR}/${DB_NAME}_$(date +'%Y-%m-%d_%H:%M:%S').sql
